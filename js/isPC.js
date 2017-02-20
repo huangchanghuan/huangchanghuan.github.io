fa391fbca6a70e4c5cc12ee25c4643db628f659e
Changhuan Huang <2464375449@qq.com>
2017-02-20 10:32:48 +0800

    modify

---
+++ js/isPC.js
@@ -0,0 +1,21 @@
+function IsPC() {
+    var userAgentInfo = navigator.userAgent;
+    var Agents = ["Android", "iPhone",
+        "SymbianOS", "Windows Phone",
+        "iPad", "iPod"];
+    var flag = true;
+    for (var v = 0; v < Agents.length; v++) {
+        if (userAgentInfo.indexOf(Agents[v]) > 0) {
+            flag = false;
+            break;
+        }
+    }
+    return flag;
+}
+
+(function(){
+    if(!IsPC()){//如果是手机端
+        var main_right=document.getElementsByClassName("main-right")
+        main_right.style.padding = "0";
+    }
+})();
