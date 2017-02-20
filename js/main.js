da4afb81ebdd8afd655580aa4d172451c107fc9a
Changhuan Huang <2464375449@qq.com>
2017-02-20 21:54:08 +0800

    modify

--- js/main.js
+++ js/main.js
@@ -1,12 +1,26 @@
 (function () {
-
   'use strict';
-
   var document = window.document;
   var Viewer = window.Viewer;
+  if( ("onhashchange" in window) && ((typeof document.documentMode==="undefined") || document.documentMode==8)) {//监控url的hash变化.(图片打开时候hash会变化)
+      // 浏览器支持onhashchange事件
+      window.onhashchange = hashChangeFire;
+  } else {
+      alert("浏览器不支持浏览图片插件!")
+  }
 
+  function hashChangeFire() {
+      //获取url,如果结尾没有hash则执行关闭窗口
+      var showPicUrl = window.location.href ;
+      if(showPicUrl.indexOf("#showPic")==-1){//如果不存在,则关闭窗口
+          var viewer_button=document.getElementsByClassName("viewer-button")[0];
+          if(viewer_button){
+              viewer_button.click();
+          }
+      }
+  }
 
-  // Photo Gallery
+    // Photo Gallery
   // -------------------------
 
   window.addEventListener('DOMContentLoaded', function () {
@@ -16,11 +30,23 @@
     var i;
     for (i = 0; i < length; i++) {
       viewers.push(new Viewer(galleries.item(i), {
-          toolbar: false,
-          navbar:false,
+        toolbar: false,
+        navbar:false,
         url: function () {
           return this.src.replace('/thumbnails', '');
-        }
+        },
+          hide:  function (e) {//点击关闭图片,处理url的hash
+              //获取url,如果结尾没有hash则执行关闭窗口
+              var showPicUrl = window.location.href;
+              if(showPicUrl.indexOf("#showPic")==-1){//如果不存在,则关闭窗口
+                  var viewer_button=document.getElementsByClassName("viewer-button")[0];
+                  if(viewer_button[0]){
+                      viewer_button[0].click();
+                  }
+              }else{//存在hash
+                  history.go(-1)
+              }
+          }
       }));
     }
 
